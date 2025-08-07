e56404e187aa83c1aa7a6b8458124b31055a252b
YURI\YURI <yuri@ex-it.co.kr>
2025-08-07 15:59:02 +0900

    작업중

--- src/app/example/page.tsx
+++ src/app/example/page.tsx
@@ -1,5 +1,6 @@
 import Kakaomap from "@/(components)/ui/kakaomap";
 import Image from "next/image";
+import Gallery from "../../(components)/ui/gallery";
 
 export default function ExamplePage() {
   const data = {
@@ -21,12 +22,17 @@ export default function ExamplePage() {
       "https://newsimg-hams.hankookilbo.com/2022/03/31/065f576b-0ff9-411d-8edb-edc139721de0.jpg",
       "https://newsimg-hams.hankookilbo.com/2022/03/31/065f576b-0ff9-411d-8edb-edc139721de0.jpg",
       "https://newsimg-hams.hankookilbo.com/2022/03/31/065f576b-0ff9-411d-8edb-edc139721de0.jpg",
+      "https://newsimg-hams.hankookilbo.com/2022/03/31/065f576b-0ff9-411d-8edb-edc139721de0.jpg",
+      "https://newsimg-hams.hankookilbo.com/2022/03/31/065f576b-0ff9-411d-8edb-edc139721de0.jpg",
+      "https://newsimg-hams.hankookilbo.com/2022/03/31/065f576b-0ff9-411d-8edb-edc139721de0.jpg",
+      "https://newsimg-hams.hankookilbo.com/2022/03/31/065f576b-0ff9-411d-8edb-edc139721de0.jpg",
+      "https://newsimg-hams.hankookilbo.com/2022/03/31/065f576b-0ff9-411d-8edb-edc139721de0.jpg",
     ],
   };
 
   return (
     <main className="flex flex-col items-center bg-[#333]">
-      <div className="card-wrapper relative max-w-[100vw] w-[44rem] bg-[#fff]">
+      <div className="overflow-hidden card-wrapper relative max-w-[100vw] w-[44rem] bg-[#fff]">
         <section className="w-full relative">
           <Image
             src="https://newsimg-hams.hankookilbo.com/2022/03/31/065f576b-0ff9-411d-8edb-edc139721de0.jpg"
@@ -37,7 +43,7 @@ export default function ExamplePage() {
             className="w-full h-full object-cover"
           />
           <div className="text-center z-10 absolute top-[80%] left-1/2 -translate-x-1/2 -translate-y-1/2 text-[#fff]">
-            <p className="text-[8em] line-height-[0.6em] pb-[4rem] font-[Parisienne]">
+            <p className="text-[8.5em] leading-[0.3em] pb-[4rem] font-[Parisienne]">
               Wedding
             </p>
             <p>
@@ -58,46 +64,73 @@ export default function ExamplePage() {
               }}
             />
           </div>
-          <div className="text-center">
-            <p>신랑부 신랑모 의 아들 신랑</p>
-            <p>신부부 신부모 의 딸 신부</p>
+          <div className="w-[60%] my-8 border-t border-[#bfa075] m-auto" />
+          <div className="text-center pt-16">
+            <p>
+              신랑부 신랑모{" "}
+              <span className="opacity-60 text-[1.4rem]">의 아들</span> 신랑
+            </p>
+            <p>
+              신부부 신부모{" "}
+              <span className="opacity-60 text-[1.4rem]">의 딸</span> 신부
+            </p>
           </div>
         </section>
         <section>
-          <div>
-            <p>
+          <div className="text-center py-8">
+            <div>
               신랑측 연락처 <a></a>
-            </p>
-            <p>
+              <a href="tel:010-1234-5678">
+                <Image
+                  src="/assets/icons/phone.svg"
+                  alt="전화"
+                  className="w-6 h-6 inline-block"
+                  width={30}
+                  height={30}
+                />
+              </a>
+              <a href="sms:010-1234-5678">
+                <Image
+                  src="/icons/message.svg"
+                  alt="문자"
+                  className="w-6 h-6 inline-block"
+                  width={30}
+                  height={30}
+                />
+              </a>
+              <div>신랑 전화 문자</div>
+            </div>
+            <div>
               신부측 연락처 <a></a>
-            </p>
+            </div>
           </div>
         </section>
         <section>
-          <div>마음 전하는 곳</div>
+          <div className="text-center py-8 mt-8">마음 전하는 곳</div>
         </section>
         <section>
           <div>
             <div className="text-center py-8 mt-8">Gallery</div>
             <div className="flex justify-center items-center flex-wrap gap-8">
-              {data.galleryItems.map((item, index) => (
-                <Image
-                  key={index}
-                  src={item}
-                  alt={`Gallery image ${index + 1}`}
-                  quality={80}
-                  width={120}
-                  height={120}
-                  className="inline-block"
-                />
-              ))}
+              <Gallery galleryItems={data.galleryItems} />
             </div>
           </div>
         </section>
         <section>
           <div className="text-center py-8 mt-8">위치</div>
-          <div>
-            <Kakaomap />
+          <Kakaomap />
+          <div className="p-16">
+            <div className="text-center py-8 mt-8">오시는 길</div>
+            <div>버스</div>
+            <div>⦁</div>
+            <div>지하철</div>
+            <div>⦁</div>
+            <div>주차</div>
+            <div>
+              안내데스크에서 주차권을 수령하실 수 있습니다.
+              <br />
+              2시간 무료 주차 가능
+            </div>
           </div>
         </section>
       </div>
